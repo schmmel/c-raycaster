d4503aea8c74c02be3b4726389c6952dd6631ae0
schimmel <sylvienuijtemans@gmail.com>
2025-03-12 12:08:15 +0100

    raycaster start

--- raycaster.c
+++ raycaster.c
@@ -4,6 +4,16 @@
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
+int screenWidth = 640;
+int screenHeight = 480;
+
+void raycast() {
+    for (int i = 0; i < screenWidth; i++) {
+        /* code */
+    }
+    
+}
+
 void loop() {
     SDL_Event e;
     SDL_zero(e);
@@ -17,16 +27,41 @@ void loop() {
             }
         }
 
+        SDL_RenderClear(renderer);
+
+        raycast();
+
         SDL_RenderPresent(renderer);
     } 
 }
 
 int main() {
+    float playerX = 2.5, playerY = 13;
+    float dirX = 0, dirY = -1;
+    float planeX = .85, planeY = 0;
+
+    int mapSize[] = {8, 8};
+    int map[] = {
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 2, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 2, 0, 1,
+        1, 0, 0, 0, 0, 2, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 2, 2, 2, 1,
+        1, 2, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1
+    };
+
+    int wallColors[] = {
+        128, 0, 128,
+        223, 12, 68,
+    };
+
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("initialization failed: %s\n", SDL_GetError());
     }
 
-    if (!SDL_CreateWindowAndRenderer("epic window :fire: :flag_tf:", 640, 480, 0, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("epic window :fire: :flag_tf:", screenWidth, screenHeight, 0, &window, &renderer)) {
         SDL_Log("create window/renderer failed: %s", SDL_GetError());
     }
 
