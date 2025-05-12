9fe16c8d69e0e90d06ab9b81364c9578a4373a88
schimmel <sylvienuijtemans@gmail.com>
2025-05-12 11:13:22 +0200

    gaycaster

--- raycaster-raylib.c
+++ raycaster-raylib.c
@@ -1,15 +1,23 @@
 #include "raylib.h"
 
+#define screenWidth 640
+#define screenHeight 480
+
 int main(void)
 {
-    InitWindow(640, 480, "raycaster");
+    InitWindow(screenWidth, screenHeight, "gaycaster");
 
     while (!WindowShouldClose())
     {
-        SetWindowSize(GetScreenWidth() + 1, 480);
         BeginDrawing();
-            ClearBackground(WHITE);
-            DrawText("a", 190, 200, 20, BLACK);
+            
+        ClearBackground(WHITE);
+
+        for (int i = 0; i <= screenWidth; i++)
+        {
+            DrawLine(i, 0, i, screenHeight, ColorFromHSV(i,1,1));
+        }
+
         EndDrawing();
     }
 
